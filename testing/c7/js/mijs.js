c6ffe1db00d264f230ab7bc0565a1187f9d6c164
SidVal <osvaldo.lopez@gmail.com>
2018-06-12 22:56:17 -0300

    clase7

---
+++ testing/c7/js/mijs.js
@@ -0,0 +1,12 @@
+// alert("Hola");
+
+function hola(){
+    alert("Pedro");
+}
+
+function saludar(nombre1){
+    var nombre1 = prompt("Pon tu nombre","");
+    if (nombre1 = "null") {
+        prompt("Pon tu nombre","");
+    } else alert("Saludos " + nombre1);
+}
