2943c4b99d72cbca56556fb6e048a21d9954ca4b
SidVal <osvaldo.lopez@gmail.com>
2021-12-17 21:40:32 -0300

    .

---
+++ codigos/css/lukas/script.js
@@ -0,0 +1,74 @@
+// Declaro variables globales
+let color = document.getElementById("color");
+
+// Función para cambiar el texto a color azul
+function colorAzul() {
+    color.style.color = "blue"
+}
+
+// Función para cambiar el texto a color rojo
+function colorRojo() {
+    color.style.color = "red"
+}
+
+// Función para cambiar el texto a color verde
+function colorVerde() {
+    color.style.color = "green"
+}
+
+// Función para cambiar el texto a color Naranja
+function colorNaranja() {
+    color.style.color = "orange"
+}
+
+// function mostrar()
+// {
+//     let elemento=document.getElementById("mensaje");
+//     if(elemento.style.display=="none")            
+//     {
+//         elemento.style.display="block";
+//     }
+//     else
+//     {
+//         elemento.style.display="none";
+//     }
+// }
+
+// function mostrar1()
+// {
+//     let elemento=document.getElementById("mensaje1");
+//     if(elemento.style.display=="none")            
+//     {
+//         elemento.style.display="block";
+//     }
+//     else
+//     {
+//         elemento.style.display="none";
+//     }
+// }
+
+// function mostrar2()
+// {
+//     let elemento=document.getElementById("mensaje2");
+//     if(elemento.style.display=="none")            
+//     {
+//         elemento.style.display="block";
+//     }
+//     else
+//     {
+//         elemento.style.display="none";
+//     }
+// }
+
+// function mostrar3()
+// {
+//     let elemento=document.getElementById("mensaje3");
+//     if(elemento.style.display=="none")            
+//     {
+//         elemento.style.display="block";
+//     }
+//     else
+//     {
+//         elemento.style.display="none";
+//     }
+// }
