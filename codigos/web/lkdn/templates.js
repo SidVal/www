c319eade9a8fa1105cced1dd51b52c54ebde69de
Waldo <SidVal@users.noreply.github.com>
2025-11-03 14:23:18 -0300

    Update proposal template text for clarity

--- codigos/web/lkdn/templates.js
+++ codigos/web/lkdn/templates.js
@@ -1,6 +1,6 @@
 const templates = {
   propuesta: 
-`Hola {{nombre}}, ¿cómo estás? Me interesa ayudarte en tu proyecto y te explico cómo estoy trabajando ahora: trabajo por resultados, así que nada de lo que me comentes a continuación tiene costo, te ofrezco análisis sin costo alguno y recién te pasaría la propuesta con costos para entregas puntuales una vez que tengo el escenario completo. 
+`Hola {{nombre}}, ¿cómo estás? Me interesa ayudarte en tu proyecto y te explico que nada de lo que me comentes a continuación tiene costo, te ofrezco análisis sin costo alguno y recién te pasaría la propuesta con costos para entregas puntuales una vez que tengo el escenario completo. 
 
 Me falta mayor información para analizar qué es lo que necesitas puntualmente sobre {{servicio}}
 En el caso que necesites una reunión de 15/30 min, se puede coordinar para que me conozcas personalmente. 
