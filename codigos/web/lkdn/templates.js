a8f33ee6a229ad705e38f512c92f104f3d92a70d
Waldo <SidVal@users.noreply.github.com>
2025-11-27 10:57:02 -0300

    Update template message for project inquiries

--- codigos/web/lkdn/templates.js
+++ codigos/web/lkdn/templates.js
@@ -1,8 +1,9 @@
 const templates = {
   propuesta: 
-`Hola {{nombre}}, ¿cómo estás? Me interesa ayudarte en tu proyecto y te explico que nada de lo que me comentes a continuación tiene costo, te ofrezco análisis sin costo alguno y recién te pasaría la propuesta con costos para entregas puntuales una vez que tengo el escenario completo. 
+`Hola {{nombre}}, ¿Cómo estás? Me interesa ayudarte en tu proyecto y te explico que nada de lo que me comentes a continuación tiene costo, te ofrezco análisis sin costo hasta que tengamos un escenario completo y resultados alcanzables. 
 
-Me falta mayor información para analizar qué es lo que necesitas puntualmente sobre {{servicio}}
+Para analizar qué es lo que necesitas puntualmente sobre {{servicio}} es necesario que me respondas con el mayor detalle posible sobre dónde estamos parados actualmente y a dónde quieres ir. 
+
 En el caso que necesites una reunión de 15/30 min, se puede coordinar para que me conozcas personalmente. 
 
 Automáticamente la plataforma te envía un privado, así que puedes responder a través de ese medio, así no tienes que complicarte con la parte de servicios y propuestas.
