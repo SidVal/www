f46be9b2f15df96c9ac92c8b4b5e48b4b8adea65
Waldo <SidVal@users.noreply.github.com>
2026-03-05 08:39:45 -0300

    Update templates.js

--- codigos/web/lkdn/templates.js
+++ codigos/web/lkdn/templates.js
@@ -1,11 +1,8 @@
 const templates = {
   propuesta: 
-`Hola {{nombre}}, 
+`Hola {{nombre}}, un gusto.
 
-Quiero proponerte algo diferente. No se trata únicamente de un servicio, sino de un enfoque único que aborda tus necesidades y objetivos de una manera personalizada y estratégica. Mi prioridad es escucharte atentamente, entender dónde estás ahora y hacia dónde quieres llevar tu proyecto.
+Recibo muchas peticiones automáticas y prefiero el trato personalizado. Para avanzar, ¿qué inquietud o meta específica tienes en mente? >
+Cuéntame y vemos si mi enfoque estratégico encaja con lo que necesitas.
 
-Estoy aquí para ofrecerte una solución con valor agregado, diseñada específicamente para superar tus expectativas. ¿Qué te interesa lograr? Cuéntame sobre tus ideas, objetivos o inquietudes. Puedes responder directamente a este mensaje, sin compromisos. Estoy aquí para escucharte.
-
-Quedo atento a tus comentarios.
-Saludos,
-Osvaldo`};
+Saludos, Osvaldo.`};
